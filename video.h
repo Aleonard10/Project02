765db6af897e70777e47f267c9fd418ccdb41fcb
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2018-02-13 21:40:57 -0800

    Update video.h

--- video.h
+++ video.h
@@ -4,17 +4,17 @@ using namespace std;
 class video
 {
     public:
-      video(string title, string url, string comment, float length, int rating);
-      void print();
-      bool longer(video* p);
-      bool alphabetical(video* p);
-      bool better(video* p);
+      video(string title, string url, string comment, float length, int rating); //prototype function for constructor
+      void print(); //prototype function for printing the output
+      bool longer(video* p); //prototype function for testing difference in length between 2 videos
+      bool alphabetical(video* p); //prototype function for testing difference in alphabetical order between 2 videos
+      bool better(video* p); //prototype function for testing difference in rating between 2 videos
     private:
-      string m_title;
-      string m_url;
-      string m_comment;
-      float m_length;
-      int m_rating;
+      string m_title; //member variable for title
+      string m_url; //member variable for url
+      string m_comment; //member variable for comment
+      float m_length; //member variable for length
+      int m_rating; //member variable for rating
 
 };
 #endif
